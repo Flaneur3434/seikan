ba3a5ca39a826cad621bd5571f0bcd259fe0ac74
Your Name <your.email@example.com>
2026-01-12 19:56:58 -0800

    GNATTest working

---
+++ wireguard-baremetal/tests/ada/crypto/tests/crypto-random-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Crypto.Random.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Crypto.Random.Test_Data.Test
+   with null record;
+
+   procedure Test_Fill_Random_3b8490 (Gnattest_T : in out Test);
+   --  crypto-random.ads:10:4:Fill_Random
+
+end Crypto.Random.Test_Data.Tests;
+--  end read only
